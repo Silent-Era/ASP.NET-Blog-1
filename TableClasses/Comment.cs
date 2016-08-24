331f8e1d76d1ca5c05de46cf734c1f0d8731f900
Dimitur Botev <botev_dimitur@abv.bg>
2016-08-24 19:08:33 +0300

    ASP.NET MVC Project File

---
+++ TableClasses/Comment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ASPBlog.TableClasses
+{
+    public class Comment
+    {
+        public Comment()
+        {
+            this.IsDeleted = false;
+            this.CommentDate = DateTime.Now;
+        }
+        [Key]
+        public int CommentId { get; set; }
+
+        public string CommentBody { get; set; }
+        [Required]
+        public DateTime CommentDate { get; set; }
+        public bool IsDeleted { get; set; }
+        [Required]
+        public Post post { get; set; }
+
+
+    }
+}
